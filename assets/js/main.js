57dec69891131a2b31334cb356ad6f3ef47f1bd7
zumonhossain <zumon15-13169@diu.edu.bd>
2022-10-20 12:43:24 +0600

    TOP BRANDS

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,9 +1,9 @@
 (function ($) {
 "use strict";
 
-// offer-active
+// sponsor-logo-active
 
-$('.offer-banner-active').owlCarousel({
+$('.sponsor-logo-active').owlCarousel({
 	loop:true,
 	margin:30,
 	navText:['<i class="fa fa-angle-left"></i>','<i class="fa fa-angle-right"></i>'],
@@ -13,137 +13,20 @@ $('.offer-banner-active').owlCarousel({
 			0:{
 					items:1
 			},
-			480:{
-					items:1
+			400:{
+					items:2
 			},
-			767:{
-					items:1
-			},
-			992:{
-					items:1
-			},
-			1200:{
-					items:1
-			}
-	}
-})
-
-// offer-banner-active
-$('.right-offer-active').owlCarousel({
-	loop:true,
-	margin:30,
-	navText:false,
-	nav:true,
-	dots:false,
-	responsive:{
-			0:{
-					items:1
-			},
-			480:{
-					items:1
-			},
-			767:{
-					items:1
-			},
-			992:{
-					items:1
-			},
-			1200:{
-					items:1
-			}
-	}
-})
-
-// featured-brand-active
-$('.featured-brand-active').owlCarousel({
-	loop:true,
-	margin:30,
-	navText:['<i class="fa fa-angle-left"></i>','<i class="fa fa-angle-right"></i>'],
-	nav:true,
-	dots:false,
-	responsive:{
-			0:{
-					items:1
-			},
-			480:{
-					items:1
-			},
-			767:{
-					items:3
-			},
-			992:{
-					items:4
-			},
-			1200:{
-					items:4
-			}
-	}
-})
-
-// Featured Product-active
-$('.featured-product-active').owlCarousel({
-	loop:true,
-	margin:30,
-	navText:['<i class="fa fa-angle-left"></i>','<i class="fa fa-angle-right"></i>'],
-	nav:true,
-	dots:false,
-	responsive:{
-			0:{
-					items:1
-			},
-			480:{
-					items:1
-			},
-			767:{
-					items:3
-			},
-			992:{
-					items:4
-			},
-			1200:{
-					items:4
-			}
-	}
-})
-
-// Featured Product-active
-$('.sponsor-active').owlCarousel({
-	loop:true,
-	margin:30,
-	navText:['<i class="fa fa-angle-left"></i>','<i class="fa fa-angle-right"></i>'],
-	nav:true,
-	dots:false,
-	responsive:{
-			0:{
-					items:1
-			},
-			480:{
-					items:1
-			},
-			767:{
-					items:3
-			},
-			992:{
+			768:{
 					items:4
 			},
 			1200:{
 					items:6
+			},
+			992:{
+					items:5
 			}
 	}
 })
-
-// Details-active
-$('.details-active').owlCarousel({
-	loop:true,
-	margin:30,
-	items:1,
-	navText:false,
-	nav:true,
-	dots:true,
-	autoplay:false,
-	dotsData:true,
-})
-
 
 // meanmenu
 $('#mobile-menu').meanmenu({
