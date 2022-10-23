59b4003f070fa0e13db50232232b110a7d3d3823
zumonhossain <zumon15-13169@diu.edu.bd>
2022-10-23 12:08:49 +0600

    product details page

--- assets/js/main.js
+++ assets/js/main.js
@@ -7,7 +7,7 @@
 $('.sponsor-logo-active').owlCarousel({
 	loop:true,
 	margin:30,
-	navText:['<i class="fa fa-angle-left"></i>','<i class="fa fa-angle-right"></i>'],
+	navText:false,
 	nav:true,
 	dots:false,
 	responsive:{
@@ -29,6 +29,42 @@ $('.sponsor-logo-active').owlCarousel({
 	}
 })
 
+
+// product-details-active
+$('.product-details-active').owlCarousel({
+	loop:true,
+	margin:30,
+	items:1,
+	navText:false,
+	nav:true,
+	dots:true,
+	autoplay:false,
+	dotsData:true,
+})
+
+// Best Sellers
+$('.best-sellers-active').owlCarousel({
+	loop:true,
+	margin:30,
+	navText:['<i class="fa fa-angle-left"></i>','<i class="fa fa-angle-right"></i>'],
+	nav:true,
+	dots:false,
+	responsive:{
+			0:{
+					items:1
+			},
+			768:{
+					items:2
+			},
+			992:{
+					items:3
+			},
+			1200:{
+					items:4
+			}
+	}
+})
+
 // meanmenu
 $('#mobile-menu').meanmenu({
 	meanMenuContainer: '.mobile-menu',
@@ -36,7 +72,6 @@ $('#mobile-menu').meanmenu({
 });
 
 //off-canvas-menu
-
 $(".menu-trigger").on("click", function() {
 	$(".off-canvas-menu, .off-canvas-overlay").addClass("active");
 	return false;
